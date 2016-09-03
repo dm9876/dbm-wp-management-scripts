aa59e0c7716e889a7d2cd16f2d0a5d2d049c7e1b
Dean Murray <deanmurray1@gmail.com>
2016-09-03 16:20:00 +1000

    added remove old backups + stub for dropbox upload

--- backup_production.sh
+++ backup_production.sh
@@ -30,3 +30,16 @@ mv -f backup.tar.gz $DATE-production-files.tar.gz
 mysqldump --opt -u $P_DBUSER -p$P_DBPASS --host $DBHOST $P_DBNAME > backup.sql
 gzip backup.sql
 mv -f backup.sql.gz $DATE-production-db.sql.gz
+
+#based on this script being run daily, remove both files which are from X days ago
+OLDDATE=$(date -d "-$DAYS_TO_KEEP days" +%Y%m%d)
+rm $OLDDATE-production-files.tar.gz
+rm $OLDDATE-production-db.sql.gz
+
+#TODO if dropbox upload is installed then
+#if it is the first of day of the month
+if [ $(date +%d) = "01" ]; then
+    echo "upload both files to dropbox"
+    echo $DATE-production-files.tar.gz
+    echo $DATE-production-db.sql.gz
+fi
